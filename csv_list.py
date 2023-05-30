98e6af26709f8f3c2dd4a8e3bcf5d72a749b844c
Pedrovskr <133998796+Pedrovskr@users.noreply.github.com>
2023-05-30 08:03:08 -0300

    Update csv_list.py

--- csv_list.py
+++ csv_list.py
@@ -5,6 +5,6 @@ def listar_conteudo(nomearquivo):
         leitor_csv = csv.reader(arquivo_csv)
         for linha in leitor_csv:
             print(linha)
-            
+
 arquivo = 'articles.csv'
 listar_conteudo(arquivo)
