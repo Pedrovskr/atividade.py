57683a278f464a24499f2b33e7e9141ae4b32d12
Pedrovskr <133998796+Pedrovskr@users.noreply.github.com>
2023-05-30 08:06:03 -0300

    Update csv_list.py

--- csv_list.py
+++ csv_list.py
@@ -1,10 +1,10 @@
 import csv
-
+import streamlit as st
 def listar_conteudo(nomearquivo):
     with open(nomearquivo, 'r') as arquivo_csv:
         leitor_csv = csv.reader(arquivo_csv)
         for linha in leitor_csv:
-            print(linha)
+            st.write(linha)
 
 arquivo = 'articles.csv'
 listar_conteudo(arquivo)
